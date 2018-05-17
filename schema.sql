abec5dced4ccc12669b97290e78f0f2da632a2ae
ahamesye <ahamesye@AHAMESYE1.auth.hpicorp.net>
2018-05-17 21:45:37 +0530

    one to many maping

--- schema.sql
+++ schema.sql
@@ -15,3 +15,28 @@ describe Employee;
 select * from Employee;
 truncate Employee;
 
+
+
+CREATE TABLE `Staff` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL,
+  `department` varchar(20) DEFAULT NULL,
+  `role` varchar(20) DEFAULT NULL,
+  `insert_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+select * from Staff;
+
+
+
+CREATE TABLE `category` (
+  `category_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`category_id`)
+);
+
+select * from category;
+truncate category;
+
+
+
---
+++ src/main/java/com/appFive/chap3/App5.java
@@ -0,0 +1,48 @@
+package com.appFive.chap3;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+import com.appFive.model.Category;
+import com.appFive.model.Product;
+
+
+public class App5 {
+
+	public static void main(String[] args) {
+		Configuration configuration = new Configuration();
+		configuration.configure("hibernate-xml-maping.cfg.xml");
+		
+		/* Building SessionFactory */
+		SessionFactory sessionFactory = configuration
+				.buildSessionFactory(new StandardServiceRegistryBuilder(
+						).applySettings(configuration.getProperties()).build());
+		Session session = sessionFactory.openSession();
+		
+		session.beginTransaction();
+		
+		
+	/*	Category category = new Category("Mobile");
+		Product pc = new Product("Readmi", "blade", 12000, category);
+		
+		
+		session.save(category);
+		session.save(pc);
+		*/
+		
+		
+		// In order to work bi directional query just rid off to string methods in category class
+		 List ProductList = session.createQuery("FROM Product").list();
+		 System.out.println((Product)ProductList.get(0));
+		 
+		session.getTransaction().commit();
+		session.close();
+		
+
+	}
+
+}
